47a61e666860e9533dc3d2b2cffd3476119005c3
StMannyAkinso <111481437+StMannyAkinso@users.noreply.github.com>
2024-07-22 11:40:19 +0100

    Update header.js

--- climate_action_plan/js/header.js
+++ climate_action_plan/js/header.js
@@ -3,7 +3,7 @@ export function renderHeader() {
   const headerContainer = document.querySelector("header");
   headerContainer.innerHTML = `
     <a href="https://uat.lambethclimatepartnership.org/" class="flex centred">
-      <img src="images/lambeth-climate-partnership.png" class="lambeth-logo" alt="Lambeth Climate Logo" />
+      <img src="Images/lambeth-climate-partnership.png" class="lambeth-logo" alt="Lambeth Climate Logo" />
     </a>
     <div class="search-container">
       <input type="text" class="search-bar" placeholder="Search..." />
