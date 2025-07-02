17af18443b4c2feeae960ef4cc9240fa26d0df9d
StMannyAkinso <111481437+StMannyAkinso@users.noreply.github.com>
2025-07-02 15:51:18 +0100

    Delete climate_action_plan directory

--- climate_action_plan/js/footer.js
+++
@@ -1,8 +0,0 @@
-// js/footer.js
-export function renderFooter() {
-  const footerContainer = document.querySelector("footer");
-  footerContainer.innerHTML = `
-    <p>&copy; Lambeth Council 2024. All rights reserved.</p>
-    <p>Designed by <a href="https://www.land-smyrna.com">Smyrna Aesthetics</a></p>
-  `;
-}
